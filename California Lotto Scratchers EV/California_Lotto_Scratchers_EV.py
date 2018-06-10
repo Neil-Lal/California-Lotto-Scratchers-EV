c79c8a3c74901d4628a59fc29faefeafe42f3d07
Neil-Lal <neillal1992@gmail.com>
2018-06-09 17:00:44 -0700

    Added packages and started the beautifulsoup web scrapper.

--- California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
+++ California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
@@ -1,4 +1,30 @@
+import pypyodbc
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-print("hello world!") 
+## Retrieve Data from Website
+    # Start with one game then later on add the rest of the variations
+    # http://www.calottery.com/play/scratchers-games
+    # http://www.calottery.com/play/scratchers-games/$1-scratchers
+    # http://www.calottery.com/play/scratchers-games/$2-scratchers
+    # http://www.calottery.com/play/scratchers-games/$3-scratchers
+    # http://www.calottery.com/play/scratchers-games/$5-scratchers
+    # http://www.calottery.com/play/scratchers-games/$10-scratchers
+    # http://www.calottery.com/play/scratchers-games/$20-scratchers
+    # http://www.calottery.com/play/scratchers-games/$30-scratchers
 
-print("coool!")
+websites = ["http://www.calottery.com/Play/Scratchers-games/$30-Scratchers/bonus-play-millions-1313"]
+for i in websites:
+    page = requests.get(i)
+    soup = bs(page.text, 'html.parser')
+    oddsAndPrizes = soup.find(class_='table.draw_games')
+    
+    df = pd.DataFrame(oddsAndPrizes)
+    
+    print(oddsAndPrizes)
+    print(df.head())
+    
+
+
+##
