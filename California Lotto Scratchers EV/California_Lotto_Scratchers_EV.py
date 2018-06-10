9ed57f8271fcbbb821fb334e5a2f1e48ef3c04e4
Neil-Lal <neillal1992@gmail.com>
2018-06-09 17:50:46 -0700

    Web scrapper complete (1 website)

--- California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
+++ California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
@@ -16,15 +16,33 @@ import pandas as pd
 
 websites = ["http://www.calottery.com/Play/Scratchers-games/$30-Scratchers/bonus-play-millions-1313"]
 for i in websites:
+    prizeList = []
     page = requests.get(i)
     soup = bs(page.text, 'html.parser')
-    oddsAndPrizes = soup.find(class_='table.draw_games')
+    oddsAndPrizes = soup.select("table.draw_games.tag_even tr")
     
-    df = pd.DataFrame(oddsAndPrizes)
-    
-    print(oddsAndPrizes)
-    print(df.head())
-    
+    title = soup.find(class_="heroContentBox")
+    name = list(title.find("h1").stripped_strings)[0]
+
+    for table_row in oddsAndPrizes:
+        cells = table_row.findAll('td')
+
+
+        if len(cells) > 0:
+            prizes = list(cells[0].stripped_strings)[0] 
+            odds = list(cells[1].stripped_strings)[0]
+            totalWinners = list(cells[2].stripped_strings)[0]
+            prizesClaimed = list(cells[3].stripped_strings)[0]
+            prizesAvailable = list(cells[4].stripped_strings)[0]
+
+            rows = {'name': name, 'prizes': prizes, 'odds': odds, 'totalWinners': totalWinners, 'prizesClaimed': prizesClaimed, 'prizesAvailable': prizesAvailable}
+            prizeList.append(rows)
+
+    df = pd.DataFrame(prizeList)
+
+##
+
+## Connect to MSSQL server
 
 
 ##
