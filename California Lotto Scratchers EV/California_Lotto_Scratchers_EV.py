16082c8de0cf99b37101b63dff91ad783232919b
Neil-Lal <neillal1992@gmail.com>
2018-06-17 02:00:45 -0700

    Working.  Multiple ticket scraping branch

--- California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
+++ California Lotto Scratchers EV/California_Lotto_Scratchers_EV.py
@@ -9,26 +9,79 @@ from random import randint
 
 ## Variables
     # Webpage to scrape
-websites = ["http://www.calottery.com/Play/Scratchers-games/$30-Scratchers/bonus-play-millions-1313"]
+#websites = ["http://www.calottery.com/Play/Scratchers-games/$30-Scratchers/bonus-play-millions-1313"]
+websites = ["http://www.calottery.com/play/scratchers-games/$1-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$2-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$3-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$5-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$10-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$20-scratchers",
+            "http://www.calottery.com/play/scratchers-games/$30-scratchers"]
+PK_ID = 0
 
+## Connect to MSSQL server and send data
+    # Takes in list of pandas dataframes and table to insert into
+    # No return; Adds data to table
+def toSQL(frame,table_name):
+    engine = sqlalchemy.create_engine('mssql+pyodbc://DESKTOP-L0JOFBL\TOPSDATA_ME/topsdata_CalLotto?driver=SQL+Server')
+    for i in frame:
+        i.to_sql(name=table_name,con=engine,if_exists='append', index=False)
+
+## Grab highest PK ID
+def grabPK():
+    engine = sqlalchemy.create_engine('mssql+pyodbc://DESKTOP-L0JOFBL\TOPSDATA_ME/topsdata_CalLotto?driver=SQL+Server')
+    con = engine.connect()
+    r = con.execute('SELECT MAX(Ticket_ID) FROM Scratchers_Prices')
+    if isinstance(r,int):
+        return r
+    else:
+        return 0
+PK_ID = grabPK()
 
 ## Retrieve Data from Website
-    # Start with one game then later on add the rest of the variations
-    # http://www.calottery.com/play/scratchers-games
-    # http://www.calottery.com/play/scratchers-games/$1-scratchers
-    # http://www.calottery.com/play/scratchers-games/$2-scratchers
-    # http://www.calottery.com/play/scratchers-games/$3-scratchers
-    # http://www.calottery.com/play/scratchers-games/$5-scratchers
-    # http://www.calottery.com/play/scratchers-games/$10-scratchers
-    # http://www.calottery.com/play/scratchers-games/$20-scratchers
-    # http://www.calottery.com/play/scratchers-games/$30-scratchers
     # Takes in a list of webpages to scrape
-    # Returns a list of pandas dataframes containing prize data
-def scrapper(websites):
-    frames = []
+    # Returns a list of pandas dataframes containing Scratcher data and prices
+def scrapper(websites,PK_ID):
+    PK_ID = PK_ID
+    framesScratchers = []
+    framesPrices = []
+    urls = []
+    keys = []
     for i in websites:
-        prizeList = []
+        tickets = []
+
         page = requests.get(i)
+        soup = bs(page.text, 'html.parser')
+        ticks = soup.find_all(class_='scratcher-small')
+
+        # Add price data - applies to each ticket in website
+        price = i.split('$')[1].split('-')[0]
+        for row in ticks:
+            tick_url = row.find_all('a',href=True)
+            tick_img = row.find_all('img')
+
+            if len(tick_url) > 0:
+                url = "http://www.calottery.com" + tick_url[0]['href']
+                urls.append(url)
+            else:
+                url = ''
+                urls.append('')
+            if len(tick_img) > 0:
+                img = tick_img[0]['src']
+            else:
+                img = ''
+                print("NO IMAGE")
+            ticket_ID = PK_ID + 1
+            keys.append(ticket_ID)
+            PK_ID += 1
+            rows = {'price': price,'url': url, 'img' : img, 'Ticket_ID': ticket_ID}
+            tickets.append(rows)
+        sleep(randint(1,2))
+        framesPrices.append(pd.DataFrame(tickets))
+    for i in range(len(urls)):
+        prizeList = []
+        key = keys[i]
+        page = requests.get(urls[i])
         soup = bs(page.text, 'html.parser')
         oddsAndPrizes = soup.select("table.draw_games.tag_even tr")
     
@@ -46,30 +99,19 @@ def scrapper(websites):
                 prizesClaimed = list(cells[3].stripped_strings)[0].replace(',','')
                 prizesAvailable = list(cells[4].stripped_strings)[0].replace(',','')
 
-                rows = {'Extract_Date': date,'Ticket_name': name, 'Prizes': prizes, 'Odds': odds, 'Total_winners': totalWinners, 'Prizes_claimed': prizesClaimed, 'Prizes_available': prizesAvailable}
+                rows = {'Ticket_ID': key, 'Extract_Date': date,'Ticket_name': name, 'Prizes': prizes, 'Odds': odds, 'Total_winners': totalWinners, 'Prizes_claimed': prizesClaimed, 'Prizes_available': prizesAvailable}
                 prizeList.append(rows)
-
-        frames.append(pd.DataFrame(prizeList))
-        sleep(randint(1,4))
-    return frames
+        
+        framesScratchers.append(pd.DataFrame(prizeList))
+        sleep(randint(1,2))
+    return [framesScratchers, framesPrices]
        
-## Connect to MSSQL server
-    # Takes in list of pandas dataframes
-    # No return; Adds data to Scratchers table
-def SQL(frames):
-    engine = sqlalchemy.create_engine('mssql+pyodbc://DESKTOP-L0JOFBL\TOPSDATA_ME/topsdata_CalLotto?driver=SQL+Server')
-    for i in frames:
-        i.to_sql(name="Scratchers",con=engine,if_exists='append', index=False)
-
-
-
 
 def main():
-    f = scrapper(websites)
-    print(f[0].head(10))
-    SQL(f)
-
-
+    f = scrapper(websites,PK_ID)
+    toSQL(f[1],'Scratchers_Prices')
+    toSQL(f[0],'Scratchers_Data')
+    
 
 
 main()
